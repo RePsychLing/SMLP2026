e563d551f45a055c4719fbd4072e427303d2eb10
Phillip Alday <me@phillipalday.com>
2026-06-21 17:48:40 +0200

    Substantial reorganization to enable better self service (#7)
    * substantial overhaul for better self-guided exploration, courtesy of Opus 4.8
    
    * add Julia version to footer
    
    * American spelling
    
    * style for objectives div
    
    * TODO
    
    * slightly more my style
    
    * sentence good
    
    * more theming tweaks
    
    * remove Grouping()
    
    * tweak some plots
    
    * edits to sleep study
    
    * suppress warnings on loading old saved fits
    
    * fix an issue in caching fallback
    
    * edits to singularity.qmd
    
    * updated fit
    
    * authors
    
    * plot tweaks
    
    * thinko
    
    * aog plots
    
    * mrk17
    
    * transformations
    
    * fggk21
    
    * kwdyz11
    
    * sleepstudy speed
    
    * permute order
    
    * bootstrap, remove kb07.qmd as redundant
    
    * profiling
    
    * power simulation
    
    * authors

--- _extensions/gitrev/git.lua
+++ _extensions/gitrev/git.lua
@@ -6,8 +6,24 @@ function git(command)
   return output
 end
 
+-- run an arbitrary shell command and return its stdout
+function shell(command)
+  local p = io.popen(command)
+  local output = p:read('*all')
+  p:close()
+  return output
+end
+
 return {
-  
+
+  -- report the version of Julia used to render the site, e.g. "1.12.6".
+  -- `julia` is always on PATH during a render because it is the notebook engine.
+  ["julia-version"] = function(args, kwargs, meta)
+    local output = shell("julia --version")
+    local version = output:match("%d+%.%d+%.%d+[%w%.%-%+]*")
+    return pandoc.Str(version or "unknown")
+  end,
+
   ["git-rev"] = function(args, kwargs, meta)
     -- command line args
     local cmdArgs = ""
