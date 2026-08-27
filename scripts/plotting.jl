f2caaf461dd66d6645cc7c8fcb2e4bbe25b2173b
Phillip Alday <me@phillipalday.com>
2026-08-27 09:14:49 +0200

    initial plotting script

---
+++ scripts/plotting.jl
@@ -0,0 +1,31 @@
+using CairoMakie # plotting backend needed for the other plotting stuff
+using AlgebraOfGraphics # ggplot2 type interface
+using MixedModelsMakie  # mixed models specials for plotting
+using DataFrames
+using Effects # like effects or emmeans
+using MixedModels # you know this one
+using MixedModelsDatasets # for the data
+
+kb07 = dataset(:kb07)
+insteval = dataset(:insteval)
+ml1m = dataset(:ml1m)
+
+mkb07 = lmm(@formula(rt_trunc ~ 1 + spkr * prec * load 
+                             + (1 + spkr + prec + load | subj)
+                             + (1 + spkr + prec + load | item)),
+            kb07;
+            contrasts=Dict(:spkr => EffectsCoding(),
+                           :prec => EffectsCoding(),
+                           :load => EffectsCoding()))
+
+mm = lmm(@formula(Y ~ 1 + (1|G) + (1|H)), dataset(:ml1m))
+mi = lmm(@formula(y ~ 1 + service + (1|s) + (1|d) + (1|dept)), dataset(:insteval))
+
+upsetplot(kb07; cols=Not([:subj, :item]))
+upsetplot(mkb07, :subj)
+upsetplot(mkb07, :item)
+nestingplot(mkb07)
+nestingtable(mkb07)
+filter(:count => ==(0), nestingtable(mkb07))
+nestingplot(mi)
+nestingplot(mm)
