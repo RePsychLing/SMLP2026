2bf6c7f41555464ec1b8b3f7ef6773c74948e986
Phillip Alday <me@phillipalday.com>
2026-08-27 09:45:55 +0200

    wip

--- scripts/plotting.jl
+++ scripts/plotting.jl
@@ -5,6 +5,7 @@ using DataFrames
 using Effects # like effects or emmeans
 using MixedModels # you know this one
 using MixedModelsDatasets # for the data
+using Random # for the random number generator
 
 kb07 = dataset(:kb07)
 insteval = dataset(:insteval)
@@ -15,7 +16,7 @@ mkb07 = lmm(@formula(rt_trunc ~ 1 + spkr * prec * load
                              + (1 + spkr + prec + load | item)),
             kb07;
             contrasts=Dict(:spkr => EffectsCoding(),
-                           :prec => EffectsCoding(),
+                           :prec => EffectsCoding(base="maintain"),
                            :load => EffectsCoding()))
 
 mi = lmm(@formula(y ~ 1 + service + (1|s) + (1|d) + (1|dept)), dataset(:insteval))
@@ -32,3 +33,31 @@ nestingplot(mm)
 upsetplot(kb07; cols=Not([:subj, :item]))
 upsetplot(mkb07, :subj)
 upsetplot(mkb07, :item)
+
+mkb07_small = lmm(@formula(rt_trunc ~ 1 + spkr * prec * load 
+                             + (1 + spkr | subj)
+                             + (1 + load | item)),
+            kb07;
+            contrasts=Dict(:spkr => EffectsCoding(),
+                           :prec => EffectsCoding(base="maintain"),
+                           :load => EffectsCoding()))
+
+bkb07 = parametricbootstrap(MersenneTwister(2708), 500, mkb07_small)
+
+coefplot(mkb07)
+
+coefplot(mkb07, mkb07_small; 
+         show_intercept=false,
+         labels=["big", "small"])
+
+coefplot(mkb07_small, bkb07; 
+         show_intercept=false,
+         labels=["wald", "boot"])
+
+ridgeplot(bkb07; show_intercept=false)
+
+ridgeplot(bkb07; ptype=:σ)
+
+ridgeplot(bkb07; 
+          ptype=:sigma, 
+          group=:subj)
